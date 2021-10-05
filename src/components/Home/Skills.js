ab68d3c4779b0ad7777a08efd17f2879a8ffd452
zottwickel <scott.g.mckell@gmail.com>
2021-10-05 16:56:27 -0600

    update skills list and optimize hero images

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -6,15 +6,31 @@ const Skills = () => {
   return (
     <Section>
       <SkillsWrapper>
-        <h1 className="skills-banner">Skills</h1>
+        <h1 className="skills-banner">Highest Proficiencies</h1>
         <div className="skill-wrapper">
           <div className="skill">
             <h2 className="skill-title">HTML5 &amp; CSS3</h2>
             <img className="skill-image" src={require("../../images/html-css.png")} alt="html/css logo" />
           </div>
           <div className="skill">
+            <h2 className="skill-title">Sass</h2>
+            <img className="skill-image" src={require("../../images/sass.svg")} alt="html/css logo" />
+          </div>
+          <div className="skill">
+            <h2 className="skill-title">TypeScript</h2>
+            <img className="skill-image" src={require("../../images/ts.png")} alt="html/css logo" />
+          </div>
+          <div className="skill">
             <h2 className="skill-title">JavaScript</h2>
             <img className="skill-image" src={require("../../images/js.jpg")} alt="html/css logo" />
+          </div>
+          <div className="skill">
+            <h2 className="skill-title">Angular</h2>
+            <img className="skill-image" src={require("../../images/ng.png")} alt="html/css logo" />
+          </div>
+          <div className="skill">
+            <h2 className="skill-title">angularJS(legacy)</h2>
+            <img className="skill-image" src={require("../../images/ajs.png")} alt="html/css logo" />
           </div>
           <div className="skill">
             <h2 className="skill-title">ReactJS</h2>
@@ -35,14 +51,6 @@ const Skills = () => {
           <div className="skill">
             <h2 className="skill-title">Ruby</h2>
             <img className="skill-image" src={require("../../images/ruby.png")} alt="html/css logo" />
-          </div>
-          <div className="skill">
-            <h2 className="skill-title">VS Code</h2>
-            <img className="skill-image" src={require("../../images/vscode.png")} alt="html/css logo" />
-          </div>
-          <div className="skill">
-            <h2 className="skill-title">Linux Shell</h2>
-            <img className="skill-image" src={require("../../images/bash.png")} alt="html/css logo" />
           </div>
         </div>
       </SkillsWrapper>
